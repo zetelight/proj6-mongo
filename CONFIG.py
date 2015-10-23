7c2d038caf15db2f7fe40227074d70b080d72146
Michal Young <michal.young@gmail.com>
2015-10-23 16:14:56 -0700

    Configured for ix (user-specific for Michal)

--- CONFIG.py
+++ CONFIG.py
@@ -6,10 +6,11 @@ Edit to fit development or deployment environment.
 import random 
 
 ### My local development environment
-PORT=5000
-DEBUG = True
-MONGO_URL = "mongodb://memo:iremember@localhost:27333/memos"  # on Gnat
+#PORT=5000
+#DEBUG = True
+#MONGO_URL = "mongodb://memo:iremember@localhost:27333/memos"  # on Gnat
 
-### On ix.cs.uoregon.edu
-#PORT=random.randint(5000,8000)
-#DEBUG = False # Because it's unsafe to run outside localhost
+### On ix.cs.uoregon.edu (Michal Young's instance of MongoDB)
+PORT=random.randint(5000,8000)
+DEBUG = False # Because it's unsafe to run outside localhost
+MONGO_URL =  "mongodb://memo:iremember@localhost:4915/memos"  # on ix
